2433f205d54f7d724e3356eb6d3d5057dfea18dd
Vikram Kangotra <vikramkangotra8055@gmail.com>
2021-12-22 23:56:45 +0530

    Fixed Matrix class and changed file structure

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include "matrix.hpp"
 
 int main() {
-    Matrix<float> matrix{4, 4};
+    Matrix<float, 4, 4> matrix;
 
     matrix.set({10, 2, 3, 4, 5, 60, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
 
