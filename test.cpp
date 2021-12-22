f5727d4253943fa6c756127bb912975f5f801bba
Vikram Kangotra <vikramkangotra8055@gmail.com>
2021-12-22 23:57:47 +0530

    Fixed Matrix class and changed file structure

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include "matrix.hpp"
-
-int main() {
-    Matrix<float, 4, 4> matrix;
-
-    matrix.set({10, 2, 3, 4, 5, 60, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
-
-    std::cout << matrix.transpose().determinant();
-}
